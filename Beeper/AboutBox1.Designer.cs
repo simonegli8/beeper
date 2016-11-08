ffe4ba25d26350f4fa8e5f5748c358272473c1f7
Simon Egli <simon@johnshope.com>
2016-11-08 23:02:56 +0100

    fix

---
+++ Beeper/AboutBox1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Beeper {
+	partial class AboutBox1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(417, 159);
+			this.label1.TabIndex = 0;
+			this.label1.Text = resources.GetString("label1.Text");
+			this.label1.Click += new System.EventHandler(this.HandleClick);
+			// 
+			// AboutBox1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(454, 181);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutBox1";
+			this.Padding = new System.Windows.Forms.Padding(9);
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "AboutBox1";
+			this.Click += new System.EventHandler(this.HandleClick);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Beeper/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+using WMPLib;
+
+
+namespace Beeper {
+
+	public class Program {
+
+		static List<string> SongExtensions = new List<string>() { ".mp3", ".wav", ".m4p", ".m4a", ".wma" };
+		static WindowsMediaPlayer Player = new WindowsMediaPlayer();
+		static NotifyIcon Icon = null;
+		static int Retry = 0;
+		static bool Exiting = false;
+		const int Mute = 2200;
+
+		static void PlayRandomFromFolder(string path) {
+			var dir = new DirectoryInfo(path);
+			if (dir.Exists) {
+				var infos = dir.GetFileSystemInfos()
+					.Where(x => x is DirectoryInfo || SongExtensions.Contains(x.Extension))
+					.ToList();
+				if (infos.Count > 0) {
+					var obj = infos[new Random().Next(infos.Count)];
+					if (obj is DirectoryInfo) PlayRandomFromFolder(obj.FullName);
+					else PlaySong(obj.FullName);
+				} else if (path != main) Play(main);
+			}
+		}
+
+		static string SongName(string path) {
+			return Regex.Replace(Path.GetFileNameWithoutExtension(path).Replace("_", " "), "[0-9]+", "");
+		}
+
+		static void Exit() {
+			lock (Icon) {
+				if (Exiting) return;
+				Exiting = true;
+			}
+			var maxvol = Player.settings.volume;
+			var vol = maxvol;
+			if (vol > 1) {
+				var delay = Mute / vol;
+				while (vol > 0) {
+					Player.settings.volume--;
+					vol--;
+					Thread.Sleep(delay);
+				}
+			}
+			Player.controls.stop();
+			Player.settings.volume = maxvol;
+			Player.close();
+			Icon.Visible = false;
+			Application.Exit();
+		}
+
+		static void PlayStateChange(int NewState) {
+			if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped) {
+				Exit();
+			}
+		}
+
+
+		static void PlaySong(string path = null, IWMPMedia item = null) {
+			if (item != null || (File.Exists(path) && SongExtensions.Contains(Path.GetExtension(path)))) {
+				try {
+					if (item != null) Player.URL = item.sourceURL;
+					else Player.URL = new Uri(path).AbsoluteUri;
+					Player.PlayStateChange += PlayStateChange;
+					Player.controls.play();
+					Thread.Sleep(2000);
+				} catch {
+				}
+				if (Player.playState != WMPPlayState.wmppsPlaying) {
+					Play(main);
+				} else {
+					var songName = "";
+					if (!string.IsNullOrEmpty(path)) songName = SongName(path);
+					if (item == null) item = Player.currentMedia;
+					if (item != null) {
+						var author = item.getItemInfo("Author");
+						var title = item.getItemInfo("Title");
+						if (author != null && title != null) songName = author + ", " + title;
+					}
+					Icon = new NotifyIcon();
+					Icon.BalloonTipTitle = "Playing:";
+					Icon.BalloonTipText = songName;
+					Icon.BalloonTipIcon = ToolTipIcon.Info;
+					Icon.Icon = new System.Drawing.Icon(typeof(Program), "StopPlaying.ico");
+					Icon.MouseClick += Icon_MouseClick;
+					Icon.ShowBalloonTip(7000);
+					Icon.Text = songName;
+					Icon.Visible = true;
+				}
+			}
+		}
+
+		private static void Icon_MouseClick(object sender, MouseEventArgs e) {
+			if (e.Button == MouseButtons.Left) {
+				Exit();
+			}
+		}
+
+		static void Play(string path) {
+			if (Retry++ > 20) Exit();
+
+			var playList = Player.playlistCollection.getByName(path);
+			IWMPPlaylist list = null;
+			if (playList != null && playList.count > 0) list = playList.Item(new Random().Next(playList.count));
+			if (list == null || list.count <= 0) list = Player.mediaCollection.getByAuthor(path);
+			if (list == null || list.count <= 0) list = Player.mediaCollection.getByGenre(path);
+			if (list == null || list.count <= 0) list = Player.mediaCollection.getByAlbum(path);
+			if (list != null && list.count > 0) PlaySong(null, list.Item[new Random().Next(list.count)]);
+			else {
+				if (Directory.Exists(path)) {
+					PlayRandomFromFolder(path);
+				} else {
+					var ext = Path.GetExtension(path);
+					if (SongExtensions.Contains(ext) && File.Exists(path)) PlaySong(path);
+				}
+			}
+		}
+
+		static string main;
+
+		public static void Main(string[] args) {
+
+			var showInfo = false;
+			string path = null;
+			if (args.Length > 0) {
+				path = args[0];
+				if (path == "-?" || path == "-help") {
+					showInfo = true;
+					path = null;
+				}
+			}
+			path = path ?? Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+			main = path;
+			Play(path);
+
+			if (showInfo) {
+				var form = new AboutBox1();
+				form.Show();
+			}
+			Application.Run();
+		}
+	}
+}
